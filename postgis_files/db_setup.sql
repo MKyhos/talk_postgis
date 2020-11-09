2c2495a18caa1a8d5d3ab024be2402be2d21bbbf
Malte Kyhos <malte.kyhos@civey.com>
2020-11-09 15:18:05 +0100

    Update db setup SQL file

--- postgis_files/db_setup.sql
+++ postgis_files/db_setup.sql
@@ -7,19 +7,12 @@ CREATE TABLE census_grid (
 	geom Geometry(Polygon, 25832)
 );
 
-
-
 CREATE TABLE census_features (
 	feature text,
 	feature_value text,
 	dataset text,
 	feature_id int PRIMARY KEY
 );
-
-
-
-
-
 
 CREATE TABLE census_dta (
 	grid_id_num int,
@@ -33,5 +26,24 @@ CREATE TABLE census_dta (
 		REFERENCES census_features(feature_id)
 );
 
+CREATE INDEX idx_census_dta_grid_id_num 
+
+-- 2. Run AFTER importing the OSM data
+
+/*
+	If the data is not yet imported, run
+
+	osm2pgsql \
+		berlin-germany.osm.pbf \
+		service=test-gis \
+		-S default.style \
+		--hstore
+*/
+
+-- 2.1 Add primary keys (no foreign keys avaiable, only spatial...)
 
 
+ALTER TABLE planet_osm_point ADD PRIMARY KEY (osm_id);
+ALTER TABLE planet_osm_polygon ADD PRIMARY KEY (osm_id);
+ALTER TABLE planet_osm_line ADD PRIMARY KEY (osm_id);
+ALTER TABLE planet_osm_roads ADD PRIMARY KEY (osm_id);
