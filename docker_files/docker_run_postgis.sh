30108e27603c3e2d4848436c2acba9c73a6b7e97
Malte Kyhos <malte.kyhos@civey.com>
2020-11-09 15:13:30 +0100

    Use env-list in docker run shell

--- docker_files/docker_run_postgis.sh
+++ docker_files/docker_run_postgis.sh
@@ -3,11 +3,10 @@
 docker run \
     --name=postgis \
     -d \
-    -e POSTGRES_USER=malte \
-    -e POSTGRES_PASS=password \
-    -e POSTGRES_DBNAME=gis \
+    --env-file=postgis-env.list \
     -p 5432:5432 \
     --network=pgnetwork \
     -v postgis_data:/var/lib/postgresql \
     --restart=always \
     kartoza/postgis
+    
